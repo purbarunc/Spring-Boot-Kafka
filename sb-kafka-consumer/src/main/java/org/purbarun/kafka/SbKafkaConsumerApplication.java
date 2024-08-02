c6bb27d42ce65ce5ac5d8a69375e368ef020918d
Purbarun Chakrabarti <purbarun@gmail.com>
2024-08-02 06:50:24 +0530

    Final Working version.

--- sb-kafka-consumer/src/main/java/org/purbarun/kafka/SbKafkaConsumerApplication.java
+++ sb-kafka-consumer/src/main/java/org/purbarun/kafka/SbKafkaConsumerApplication.java
@@ -1,22 +1,7 @@
 package org.purbarun.kafka;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.purbarun.kafka.model.OrderMessage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ContainerProperties;
-import org.springframework.kafka.listener.ContainerProperties.AckMode;
-import org.springframework.kafka.support.converter.JsonMessageConverter;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.messaging.support.GenericMessage;
 
 @SpringBootApplication
 public class SbKafkaConsumerApplication {
--- sb-kafka-consumer/src/main/java/org/purbarun/kafka/service/KafkaConsumer.java
+++ sb-kafka-consumer/src/main/java/org/purbarun/kafka/service/KafkaConsumer.java
@@ -3,16 +3,16 @@ package org.purbarun.kafka.service;
 import org.purbarun.kafka.model.OrderMessage;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.support.GenericMessage;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class KafkaConsumer {
 	@KafkaListener(groupId = "NEW_ORDER_group", topics = "NEW_ORDER")
 	public void consume(OrderMessage orderMessage,Acknowledgment acknowledgment) {
-		System.out.println(orderMessage);
+		log.info("Order Received from Kafka => {}", orderMessage);
 		acknowledgment.acknowledge();
 	}
 }
